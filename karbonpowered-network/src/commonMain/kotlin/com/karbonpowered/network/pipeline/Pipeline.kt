1a08d157aa4441ea16d581b7afc20eb9eb99fcd4
XjCyan1de <xjcyan1de@yandex.ru>
2021-03-21 11:30:57 +0300

    cleanup

--- karbonpowered-network/src/commonMain/kotlin/com/karbonpowered/network/pipeline/Pipeline.kt
+++
@@ -1,9 +0,0 @@
-package com.karbonpowered.network.pipeline
-
-import io.ktor.utils.io.core.*
-
-interface Pipeline {
-    suspend operator fun invoke(input: Input)
-
-    suspend operator fun invoke(output: Output)
-}
