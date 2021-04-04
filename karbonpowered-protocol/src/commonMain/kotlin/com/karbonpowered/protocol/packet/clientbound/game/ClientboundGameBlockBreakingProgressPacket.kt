490f33414f377740112e130a33fa9026a55b6334
cororo <artem.cororo@gmail.com>
2021-04-04 19:31:08 +0300

    fix

--- karbonpowered-protocol/src/commonMain/kotlin/com/karbonpowered/protocol/packet/clientbound/game/ClientboundGameBlockBreakingProgressPacket.kt
+++ karbonpowered-protocol/src/commonMain/kotlin/com/karbonpowered/protocol/packet/clientbound/game/ClientboundGameBlockBreakingProgressPacket.kt
@@ -15,16 +15,19 @@ import kotlin.reflect.KClass
 
 data class ClientboundGameBlockBreakingProgressPacket(
     val entityId: Int,
-    val blockPos: IntVector3,
-    val progress: Byte
+    val x: Int,
+    val y: Int,
+    val z: Int,
+    val progress: Int
 ) : MinecraftPacket {
+    constructor(entityId: Int, blockPos: IntVector3, progress: Int) : this(entityId, blockPos.x, blockPos.y, blockPos.z, progress)
     companion object : MessageCodec<ClientboundGameBlockBreakingProgressPacket> {
         override val messageType = ClientboundGameBlockBreakingProgressPacket::class
 
         override fun encode(output: Output, data: ClientboundGameBlockBreakingProgressPacket) {
             output.writeVarInt(data.entityId)
-            output.writeBlockPosition(data.blockPos.x, data.blockPos.y, data.blockPos.z)
-            output.writeByte(data.progress)
+            output.writeBlockPosition(data.x, data.y, data.z)
+            output.writeByte(data.progress.toByte())
         }
 
         override fun decode(input: Input): ClientboundGameBlockBreakingProgressPacket {
@@ -33,7 +36,7 @@ data class ClientboundGameBlockBreakingProgressPacket(
             val location =
                 intVector3Of(unpackLongX(packedLocation), unpackLongY(packedLocation), unpackLongZ(packedLocation))
             val progress = input.readByte()
-            return ClientboundGameBlockBreakingProgressPacket(entityId, location, progress)
+            return ClientboundGameBlockBreakingProgressPacket(entityId, location, progress.toInt())
         }
     }
 }
