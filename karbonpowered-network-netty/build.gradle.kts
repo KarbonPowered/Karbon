474ffffd7d20798d051e4db3a2f067b8d831368c
Andrey Pfau <a.martynov@adguard.com>
2021-05-20 12:30:21 +0300

    remove io_uring

--- karbonpowered-network-netty/build.gradle.kts
+++ karbonpowered-network-netty/build.gradle.kts
@@ -3,13 +3,9 @@ kotlin {
         val jvmMain by getting {
             dependencies {
                 val nettyVersion: String by project
-                val nettyIoUringVersion: String by project
                 implementation("io.netty:netty-transport:$nettyVersion")
                 implementation("io.netty:netty-codec:$nettyVersion")
                 implementation("io.netty:netty-transport-native-epoll:$nettyVersion")
-                if (org.gradle.internal.os.OperatingSystem.current().isLinux) {
-                    implementation("io.netty:netty-incubator-transport-native-io_uring:0.0.5.Final")
-                }
                 implementation(project(":karbonpowered-network"))
             }
         }
