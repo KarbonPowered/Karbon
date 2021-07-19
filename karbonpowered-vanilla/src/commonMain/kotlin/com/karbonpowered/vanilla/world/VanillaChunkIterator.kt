d93f734025da0f3b7c24373e32707f26c1669b21
Andrey Pfau <andreypfau@yandex.ru>
2021-07-19 14:21:42 +0300

    fixed chunk iterator

--- karbonpowered-vanilla/src/commonMain/kotlin/com/karbonpowered/vanilla/world/VanillaChunkIterator.kt
+++ karbonpowered-vanilla/src/commonMain/kotlin/com/karbonpowered/vanilla/world/VanillaChunkIterator.kt
@@ -8,13 +8,46 @@ class VanillaChunkIterator(
     val minY: Int,
     val maxY: Int
 ) : ChunkIterator {
-    override fun iterator(centerX: Int, centerY: Int, centerZ: Int): Iterator<IntVector3> = iterator {
-        for (x in centerX - distance..centerX + distance) {
-            for (z in centerZ - distance..centerZ + distance) {
-                for (y in minY..maxY) {
-                    yield(IntVector3(x, y, z))
+    override fun iterator(centerX: Int, centerY: Int, centerZ: Int): Iterator<IntVector3> =
+        IteratorImpl(centerX, minY, centerZ)
+
+    private inner class IteratorImpl(
+        var x: Int,
+        var y: Int,
+        var z: Int
+    ) : Iterator<IntVector3> {
+        val maxChunks = (distance * 2 + 1) * (distance * 2 + 1)
+        var dx = 1
+        var dz = 0
+        var segmentPassed = 0
+        var segmentLength = 1
+        var steps = 1
+
+        override fun hasNext(): Boolean = steps < maxChunks
+
+        override fun next(): IntVector3 {
+            val vector = IntVector3(x, y, z)
+            y += 1
+
+            if (y > maxY) {
+                segmentPassed++
+                y = minY
+                x += dx
+                z += dz
+                steps++
+            }
+
+            if (segmentPassed == segmentLength) {
+                segmentPassed = 0
+                val b = dx
+                dx = -dz
+                dz = b
+                if (dx == 0) {
+                    segmentLength++
                 }
             }
+
+            return vector
         }
     }
 }
